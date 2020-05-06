805bb6e794c1b60f8b45a35f18b3e918f258c046
tobiashg <harly.grunewald@gmail.com>
2020-05-06 11:43:56 +0200

    Create TableReservationContext.cs

---
+++ GoldenIce/Models/TableReservationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldenIce.Models
+{
+    public class TableReservationContext : DbContext
+    {
+        public TableReservationContext(DbContextOptions<TableReservationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TableReservation> TableReservations { get; set; }
+    }
+}
